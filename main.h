8aed926cdcfc0fdfd09bbf8cef28292921faf475
louisgrosse <104017639+louisgrosse@users.noreply.github.com>
2022-05-13 11:45:38 +0200

    Add files via upload

---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "camera/dcmi_camera.h"
+#include "msgbus/messagebus.h"
+#include "parameter/parameter.h"
+
+#define ROTATION_THRESHOLD		0.15
+#define ROTATION_COEFF			100     // convert rotation speed to be between 100 and 300
+#define PXTOCM					1570.0f //experimental value
+//#define GOAL_AMPLITUDE 			200000
+#define ERROR_THRESHOLD			15000
+#define KP						600.0f
+#define KI 						3.5f	//must not be zero
+#define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
+#define MAX_FREQ 30
+#define goal_amplitude 			110000
+#define turn_around 			800
+#define correction 				10000
+#define speed_conversion 		4  		//convert the speed given by the pi_regulator to be between 400 and 800
+#define prox_distance			125
+#define prox_speed				600
+
+/** Robot wide IPC bus. */
+extern messagebus_t bus;
+
+extern parameter_namespace_t parameter_root;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
