2e86b03817369d92ac9b4e45ecb09ad23a19949e
Louis Grosse <louis.grosse@epfl.ch>
2022-05-15 20:52:00 +0200

    commentary update

--- main.h
+++ main.h
@@ -9,23 +9,32 @@ extern "C" {
 #include "msgbus/messagebus.h"
 #include "parameter/parameter.h"
 
+//Defines the speed and rotation speed
 #define ROTATION_THRESHOLD		0.15f
-#define ROTATION_COEFF			300     // convert rotation speed to be between 100 and 300
-#define PXTOCM					1570.0f //experimental value
+#define ROTATION_COEFF			300     // converts rotation speed to be between 100 and 300
+
 #define ERROR_THRESHOLD			15000
+
+//PID options (defines the reactivity of the application)
 #define KP						700.0f
-#define KI 						3.5f	//must not be zero
+#define KI 						3.5f
 #define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
-#define MOV_FREQ 				75
 #define goal_amplitude 			140000
-#define correction 				10000
-#define speed_conversion 		8  		//convert the speed given by the pi_regulator to be between 400 and 800
+#define correction 				10000 //changes the order of magnitude of the microphones
+
+//Thresholds
+#define MOV_FREQ 				75		//Frequency threshold
+#define MIN_VALUE_THRESHOLD 	10000   //Amplitude threshold
+#define PHASE_THRESHOLD			4.5f	//Phase threshold
+
+
+#define speed_conversion 		8  		//convert the speed given by the pi_regulator to be between 200 and 600
+
+//Defines the reactivity and speed of the proximity sensors
 #define prox_distance_front		125
 #define prox_distance_side		25
 #define prox_distance			60
 #define prox_speed				300
-#define PHASE_THRESHOLD			4.5f	//Phase threshold
-#define MIN_VALUE_THRESHOLD 	10000   //Amplitude threshold
 
 /** Robot wide IPC bus. */
 extern messagebus_t bus;
